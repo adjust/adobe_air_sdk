c069346c479d52f9ba0451d8b1cc56aef6249a24
Andrew Slotin <andrew.slotin@gmail.com>
2014-03-26 12:52:23 +0100

    Version 2.2.1

--- ext/ios/src/AdjustIoExtension/AdjustIoExtension/AdjustIoExtension.m
+++ ext/ios/src/AdjustIoExtension/AdjustIoExtension/AdjustIoExtension.m
@@ -11,30 +11,30 @@ void ContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, u
 {
     *numFunctionsToTest = 5;
     FRENamedFunction* func = (FRENamedFunction*) malloc(sizeof(FRENamedFunction) * 5);
-    
+
     func[0].name = (const uint8_t*) "appDidLaunch";
     func[0].functionData = NULL;
     func[0].function = &appDidLaunch;
-    
+
     func[1].name = (const uint8_t*) "onPause";
     func[1].functionData = NULL;
     func[1].function = &onPause;
-    
-    
+
+
     func[2].name = (const uint8_t*) "onResume";
     func[2].functionData = NULL;
     func[2].function = &onResume;
-    
-    
+
+
     func[3].name = (const uint8_t*) "trackEvent";
     func[3].functionData = NULL;
     func[3].function = &trackEvent;
-    
-    
+
+
     func[4].name = (const uint8_t*) "trackRevenue";
     func[4].functionData = NULL;
     func[4].function = &trackRevenue;
-    
+
     *functionsToSet = func;
 }
 
@@ -44,7 +44,7 @@ void ContextFinalizer(FREContext ctx) {
 
 void AdjustIoExtensionInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet)
 {
-    [AdjustIo setSdkPrefix:@"air2.2.0"];
+    [AdjustIo setSdkPrefix:@"air2.2.1"];
 
     *extDataToSet = NULL;
     *ctxInitializerToSet = &ContextInitializer;
