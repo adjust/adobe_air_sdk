dc618c6a86535f1296e74c54eb3b14c41048e77e
Obaied <abdullah.obaied@gmail.com>
2018-01-02 16:38:18 +0100

    Update test scripts
    Simple formatting changes to make things uniform across other platforms

--- scripts/air_testing_android.sh
+++ scripts/air_testing_android.sh
@@ -1,43 +1,38 @@
 #!/usr/bin/env bash
 
-SAMPLE_DIR=${ADOBE_AIR_SDK_DIR}/example
+# Get the current directory (/scripts/ directory)
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Traverse up to get to the root directory
+ROOT_DIR="$(dirname "$SCRIPTS_DIR")"
+EXAMPLE_DIR=example
+EXT_DIR=ext/android
+VERSION=`cat ${ROOT_DIR}/VERSION`
+
 MAIN_FILE=Main.as
 SAMPLE_APP_XML_FILE=Main-app.xml
-VERSION=`cat ${ADOBE_AIR_SDK_DIR}/VERSION`
-KEYSTORE_FILE=`cd ${SAMPLE_DIR}; find . -name "*.pfx" -print` # Get any keystore file with extension .pfx
+KEYSTORE_FILE=`cd ${EXAMPLE_DIR}; find . -name "*.pfx" -print` # Get any keystore file with extension .pfx
 
 RED='\033[0;31m' # Red color
 GREEN='\033[0;32m' # Green color
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}>>> Update submodules"
-git submodule update --init --recursive
+cd ${ROOT_DIR}
 
-echo -e "${GREEN}>>> Removing ANE file from example/lib ${NC}"
-rm -rfv ${SAMPLE_DIR}/lib/Adjust-*.*.*.ane
+echo -e "${GREEN}>>> Removing ANE file from example app ${NC}"
+rm -rfv ${EXAMPLE_DIR}/lib/Adjust-*.*.*.ane
 
 echo -e "${GREEN}>>> Removing ANE file from root dir ${NC}"
-rm -rfv ${ADOBE_AIR_SDK_DIR}/Adjust*.ane
+rm -rfv Adjust*.ane
 
-echo -e "${GREEN}>>> Building ANE for version ${VERSION} ${NC}"
+echo -e "${GREEN}>>> Building ANE ${NC}"
+${SCRIPTS_DIR}/build.sh
 
-cd ${ADOBE_AIR_SDK_DIR}
-./build.sh
-mkdir -p ${SAMPLE_DIR}/lib
-cp -v Adjust-${VERSION}.ane ${SAMPLE_DIR}/lib/
+echo -e "${GREEN}>>> Copying ANE to example app ${NC}"
+mkdir -p ${EXAMPLE_DIR}/lib
+cp -v Adjust-${VERSION}.ane ${EXAMPLE_DIR}/lib/
 
-echo -e "${GREEN}>>> Checking if ANE is built successfully in location: ${SAMPLE_DIR}/lib/Adjust-${VERSION}.ane ${NC}"
-
-if [ ! -f "${SAMPLE_DIR}/lib/Adjust-${VERSION}.ane" ]; then
-    echo -e "${RED}>>> Bulding ANE failed ${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}>>> ANE built successfully ${NC}"
-
-echo -e "${GREEN}>>> Building example app ${NC}"
 echo -e "${GREEN}>>> Running amxmlc ${NC}"
-cd ${SAMPLE_DIR}
+cd ${EXAMPLE_DIR}
 amxmlc -external-library-path+=lib/Adjust-${VERSION}.ane -output=Main.swf -- ${MAIN_FILE}
 
 echo -e "${GREEN}>>> Checking if keystore exists ${NC}"
@@ -49,10 +44,13 @@ fi
 
 echo -e "${GREEN}>>> Keystore file exists ${NC}"
 
-echo -e "${GREEN}>>> Packaging APK file. Password will enter automatically ${NC}"
+echo -e "${GREEN}>>> Packaging APK file. Password will be entered automatically ${NC}"
 echo "pass" | adt -package -target apk-debug -storetype pkcs12 -keystore sampleCert.pfx Main.apk Main-app.xml Main.swf -extdir lib
 
-echo -e "${GREEN}>>> APK file created. Running ADB install ${NC}"
-adb install -r Main.apk
+echo -e "${GREEN}>>> Running adb uninstall ${NC}"
+adb uninstall com.adjust.examples
+
+#echo -e "${GREEN}>>> APK file created. Running ADB install ${NC}"
+#adb install -r Main.apk
 
 echo -e "${GREEN}>>> ADB installed ${NC}"
--- scripts/air_testing_ios.sh
+++ scripts/air_testing_ios.sh
@@ -1,44 +1,41 @@
 #!/usr/bin/env bash
 
-SAMPLE_DIR=${ADOBE_AIR_SDK_DIR}/example
+# Get the current directory (/scripts/ directory)
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Traverse up to get to the root directory
+ROOT_DIR="$(dirname "$SCRIPTS_DIR")"
+EXAMPLE_DIR=example
+EXT_DIR=ext/android
+VERSION=`cat ${ROOT_DIR}/VERSION`
+
 MAIN_FILE=Main.as
-SAMPLE_APP_XML_FILE=Main-app.xml
-VERSION=`cat ${ADOBE_AIR_SDK_DIR}/VERSION`
+EXAMPLE_APP_XML_FILE=Main-app.xml
+KEYSTORE_FILE=`cd ${EXAMPLE_DIR}; find . -name "*.pfx" -print` # Get any keystore file with extension .pfx
 
 RED='\033[0;31m' # Red color
 GREEN='\033[0;32m' # Green color
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}>>> Update submodules"
-git submodule update --init --recursive
+cd ${ROOT_DIR}
 
-echo -e "${GREEN}>>> Removing ANE file from example/lib ${NC}"
-rm -rfv ${SAMPLE_DIR}/lib/Adjust-*.*.*.ane
+echo -e "${GREEN}>>> Removing ANE file from example app ${NC}"
+rm -rfv ${EXAMPLE_DIR}/lib/Adjust-*.*.*.ane
 
 echo -e "${GREEN}>>> Removing ANE file from root dir ${NC}"
-rm -rfv ${ADOBE_AIR_SDK_DIR}/Adjust*.ane
+rm -rfv Adjust*.ane
 
-echo -e "${GREEN}>>> Building ANE for version ${VERSION} ${NC}"
+echo -e "${GREEN}>>> Building ANE ${NC}"
+${SCRIPTS_DIR}/build.sh
 
-cd ${ADOBE_AIR_SDK_DIR}
-./build.sh
-cp -v Adjust-${VERSION}.ane ${SAMPLE_DIR}/lib/
+echo -e "${GREEN}>>> Copying ANE to example app ${NC}"
+mkdir -p ${EXAMPLE_DIR}/lib
+cp -v Adjust-${VERSION}.ane ${EXAMPLE_DIR}/lib/
 
-echo -e "${GREEN}>>> Checking if ANE is built successfully in location: ${SAMPLE_DIR}/lib/Adjust-${VERSION}.ane ${NC}"
-
-if [ ! -f "${SAMPLE_DIR}/lib/Adjust-${VERSION}.ane" ]; then
-    echo -e "${RED}>>> Bulding ANE failed ${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}>>> ANE built successfully ${NC}"
-
-echo -e "${GREEN}>>> Building example app ${NC}"
 echo -e "${GREEN}>>> Running amxmlc ${NC}"
-cd ${SAMPLE_DIR}
+cd ${EXAMPLE_DIR}
 amxmlc -external-library-path+=lib/Adjust-${VERSION}.ane -output=Main.swf -- ${MAIN_FILE}
 
 echo -e "${GREEN}>>> Packaging IPA file${NC}"
-echo | adt -package -target ipa-debug -provisioning-profile ${DEV_PROVISIONING_PROFILE_PATH} -storetype pkcs12 -keystore ${KEYSTORE_FILE_PATH} Main.ipa ${SAMPLE_APP_XML_FILE} Main.swf -extdir lib
+echo | adt -package -target ipa-debug -provisioning-profile ${DEV_ADOBE_PROVISIONING_PROFILE_PATH} -storetype pkcs12 -keystore ${KEYSTORE_FILE_PATH} Main.ipa ${EXAMPLE_APP_XML_FILE} Main.swf -extdir lib
 
 echo -e "${GREEN}>>> IPA file created. ${NC}"
