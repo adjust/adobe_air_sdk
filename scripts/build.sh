13554f79a4f5e0311aeb6470b9e151f05d41966f
Obaied <abdullah.obaied@gmail.com>
2018-01-02 16:29:52 +0100

    Relocate and modify build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #---------- Directories
-# Get the current directory (ext/iOS/)
-OUT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Get the current directory (/scripts)
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Traverse up to get to the root directory
+ROOT_DIR="$(dirname "$SCRIPTS_DIR")"
 SOURCE_DIR=src
 BUILD_DIR=build
 EXT_DIR=ext
@@ -10,44 +12,40 @@ EXT_DIR=ext
 ADT=${AIR_SDK_PATH}/bin/adt
 COMPC=${AIR_SDK_PATH}/bin/compc
 COMPC_CLASSES="com.adjust.sdk.Adjust com.adjust.sdk.LogLevel com.adjust.sdk.Environment com.adjust.sdk.AdjustConfig com.adjust.sdk.AdjustAttribution com.adjust.sdk.AdjustEventSuccess com.adjust.sdk.AdjustEventFailure com.adjust.sdk.AdjustEvent com.adjust.sdk.AdjustSessionSuccess com.adjust.sdk.AdjustSessionFailure"
-VERSION=`cat VERSION`
+VERSION=`cat ${ROOT_DIR}/VERSION`
 
 #----------- echo colors
 RED='\033[0;31m' # Red color
 GREEN='\033[0;32m' # Green color
 NC='\033[0m' # No Color
 
-# cd to the called directory to be able to run the script from anywhere
-cd $(dirname $0) 
-cd ${OUT_DIR}
-
 echo -e "${GREEN}>>> AA build script: BEGIN${NC}"
-cd ${OUT_DIR}
 
-#----------- emulator
+#----------- 
 echo -e "${GREEN}>>> AA build script: running emulator tasks${NC}"
+cd ${ROOT_DIR}
 mkdir -p ${BUILD_DIR}/default
 ${COMPC} -source-path default/src -swf-version 27 -external-library-path ${AIR_SDK_PATH}/frameworks/libs/air/airglobal.swc -include-classes ${COMPC_CLASSES} -directory=true -output ${BUILD_DIR}/default
 rm -rf ${BUILD_DIR}/default/catalog.xml
 
-#----------- Run android and iOS build scripts
+#----------- 
 echo -e "${GREEN}>>> AA build script:  Running Android and iOS build scripts${NC}"
-${EXT_DIR}/Android/build.sh
-${EXT_DIR}/iOS/build.sh
+${EXT_DIR}/android/build.sh release
+${EXT_DIR}/ios/build.sh
 
-#----------- Copy generated files to BUILD_DIR
+#----------- 
 echo -e "${GREEN}>>> AA build script: copying generated files to ${BUILD_DIR} ${NC}"
-cd ${OUT_DIR}
+cd ${ROOT_DIR}
 mkdir -p ${BUILD_DIR}/Android
-cp -v ${EXT_DIR}/Android/*.jar ${BUILD_DIR}/Android
-cp -vr ${EXT_DIR}/iOS/*.a ${EXT_DIR}/iOS/*.framework ${BUILD_DIR}/iOS
+cp -vr ${EXT_DIR}/Android/*.jar ${BUILD_DIR}/Android
+cp -vr ${EXT_DIR}/ios/*.a ${EXT_DIR}/ios/*.framework ${BUILD_DIR}/iOS
 
-#------------ Making swc file
+#------------ 
 echo -e "${GREEN}>>> AA build script:  Making swc file${NC}"
 mkdir -p ${BUILD_DIR}
 ${COMPC} -source-path src -swf-version 27 -external-library-path ${AIR_SDK_PATH}/frameworks/libs/air/airglobal.swc -include-classes ${COMPC_CLASSES} -output ${BUILD_DIR}/Adjust.swc
 
-#------------ Running ADT and finalizing the ANE file 
+##------------ 
 echo -e "${GREEN}>>> AA build script: Running ADT and finalizing the ANE file ${NC}"
 unzip -d ${BUILD_DIR}/Android -qq -o ${BUILD_DIR}/Adjust.swc -x catalog.xml
 unzip -d ${BUILD_DIR}/iOS -qq -o ${BUILD_DIR}/Adjust.swc -x catalog.xml
